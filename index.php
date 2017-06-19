7e5759d4e2eee5b7094694f3b028ed641edd9ebd
ernesti94 <ernestjedrzejczyk@gmail.com>
2017-06-19 15:26:45 +0200

    szablon

--- index.php
+++ index.php
@@ -1,3 +1,121 @@
-<?php
-echo "hello world!";
-?>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-2" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <title>Sklep sportowy</title>
+    <link rel="icon" href="http://example.com/favicon.png">
+</head>
+<body>
+<div class="jumbotron">
+    <div class="dropdown-header"><h2>Internetowy sklep sportowy</h2></div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-2">
+            <h4>West Ham United</h4>
+            <img src="products/whu.jpg" class="img-responsive"/>
+
+            <p>Cena: 99,99 PLN<br>
+                Rozmiar:
+                <select class="form-control-sm" id="sel1">
+                    <option></option>
+                    <option>S</option>
+                    <option>M</option>
+                    <option>L</option>
+                    <option>XL</option>
+                </select><br>
+            Ilość dostępnych sztuk: 10</br>
+                Kod produktu: x2137jp2</br></br></p>
+            <a href="#" class="btn btn-info btn-md" style="align: center">
+                <span class="glyphicon glyphicon-shopping-cart"></span> Dodaj do Koszyka
+            </a>
+        </div>
+        <div class="col-sm-2">
+            <h4>West Ham United</h4>
+            <img src="products/whu.jpg" class="img-responsive"/>
+
+            <p>Cena: 99,99 PLN<br>
+                Rozmiar:
+                <select class="form-control-sm" id="sel1">
+                    <option></option>
+                    <option>S</option>
+                    <option>M</option>
+                    <option>L</option>
+                    <option>XL</option>
+                </select><br>
+                Ilość dostępnych sztuk: 10</br>
+                Kod produktu: x2137jp2</br></br></p>
+            <a href="#" class="btn btn-info btn-md" style="align: center">
+                <span class="glyphicon glyphicon-shopping-cart"></span> Dodaj do Koszyka
+            </a>
+        </div>
+
+        <div class="col-sm-2">
+            <h4>West Ham United</h4>
+            <img src="products/whu.jpg" class="img-responsive"/>
+
+            <p>Cena: 99,99 PLN<br>
+                Rozmiar:
+                <select class="form-control-sm" id="sel1">
+                    <option></option>
+                    <option>S</option>
+                    <option>M</option>
+                    <option>L</option>
+                    <option>XL</option>
+                </select><br>
+                Ilość dostępnych sztuk: 10</br>
+                Kod produktu: x2137jp2</br></br></p>
+            <a href="#" class="btn btn-info btn-md" style="align: center">
+                <span class="glyphicon glyphicon-shopping-cart"></span> Dodaj do Koszyka
+            </a>
+        </div>
+        <div class="col-sm-2">
+            <h4>West Ham United</h4>
+            <img src="products/whu.jpg" class="img-responsive"/>
+
+            <p>Cena: 99,99 PLN<br>
+                Rozmiar:
+                <select class="form-control-sm" id="sel1">
+                    <option></option>
+                    <option>S</option>
+                    <option>M</option>
+                    <option>L</option>
+                    <option>XL</option>
+                </select><br>
+                Ilość dostępnych sztuk: 10</br>
+                Kod produktu: x2137jp2</br></br></p>
+            <a href="#" class="btn btn-info btn-md" style="align: center">
+                <span class="glyphicon glyphicon-shopping-cart"></span> Dodaj do Koszyka
+            </a>
+        </div>
+        <div class="col-sm-2">
+            <h4>West Ham United</h4>
+            <img src="products/whu.jpg" class="img-responsive"/>
+
+            <p>Cena: 99,99 PLN<br>
+                Rozmiar:
+                <select class="form-control-sm" id="sel1">
+                    <option></option>
+                    <option>S</option>
+                    <option>M</option>
+                    <option>L</option>
+                    <option>XL</option>
+                </select><br>
+                Ilość dostępnych sztuk: 10</br>
+                Kod produktu: x2137jp2</br></br></p>
+            <a href="#" class="btn btn-info btn-md" style="align: center">
+                <span class="glyphicon glyphicon-shopping-cart"></span> Dodaj do Koszyka
+            </a>
+        </div>
+        <div class="col-sm-1"></div>
+    </div><br>
+</div>
+<div class="panel-footer">
+    <br><br>
+    <p style="text-align: center">Copyright &copy; 2017 Ernest Jędrzejczyk<p>
+    <br>
+</div>
+</body>
+</html>
